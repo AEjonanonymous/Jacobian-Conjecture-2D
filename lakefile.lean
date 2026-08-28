fca55cd2e2c640a5c51a7b74f49788f9099275d3
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-08-27 22:40:30 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package «JacobianConjecture2D» where
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
 lean_lib «JacobianConjecture2D» where
   srcDir := "."
